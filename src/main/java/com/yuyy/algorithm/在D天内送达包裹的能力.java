0dba286f20aae5ec5dd166ec7514a75cb9f6edad
yuyouyang <yuyouyang@thunisoft.com>
2020-06-02 10:15:58 +0800

    1

--- src/main/java/com/yuyy/algorithm/在D天内送达包裹的能力.java
+++ src/main/java/com/yuyy/algorithm/在D天内送达包裹的能力.java
@@ -1,13 +1,17 @@
 package com.yuyy.algorithm;
 
+import org.junit.jupiter.api.Test;
+
 public class 在D天内送达包裹的能力 {
 
     public int shipWithinDays(int[] weights, int D) {
         int sum=0;
+        int max=0;
         for (int i = 0; i < weights.length; i++) {
             sum+=weights[i];
+            max=max<weights[i]?weights[i]:max;
         }
-        int low=weights[weights.length-1];
+        int low=max;
         int heigh=sum;
         while(low<=heigh){
             int mid=low+((heigh-low)>>1);
@@ -22,7 +26,7 @@ public class 在D天内送达包裹的能力 {
 
     public static int fun(int minWeight,int[] weights){
         int weight=0;
-        int days=0;
+        int days=1;
         for (int i=0;i<weights.length;i++){
             if((weight+weights[i])>minWeight){
                 days++;
@@ -34,5 +38,13 @@ public class 在D天内送达包裹的能力 {
         return days;
     }
 
-
+    @Test
+    public void test(){
+//        int[] arr=new int[]{1,2,3,4,5,6,7,8,9,10};
+        int[] arr=new int[]{1,2,3,1,1};
+//        int d=5;
+        int d=4;
+        在D天内送达包裹的能力 clazz = new 在D天内送达包裹的能力();
+        System.out.println(clazz.shipWithinDays(arr,d));
+    }
 }
