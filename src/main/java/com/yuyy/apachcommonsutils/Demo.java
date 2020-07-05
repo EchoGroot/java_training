6cc56b5686cd4089edf09d4316bbe027a12b5f65
EchoGroot <326018984@qq.com>
2020-07-05 22:42:17 +0800

    1

---
+++ src/main/java/com/yuyy/apachcommonsutils/Demo.java
@@ -0,0 +1,48 @@
+package com.yuyy.apachcommonsutils;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ClassUtils;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Demo {
+    public static void main(String[] args) {
+        testClassUtils();
+    }
+
+    private static void testClassUtils(){
+        System.out.println(ClassUtils.getShortClassName(Demo.class));
+        System.out.println(ClassUtils.getSimpleName(Demo.class));
+        System.out.println(ClassUtils.getName(Demo.class));
+        System.out.println(ClassUtils.getPackageName(Demo.class));
+    }
+
+    // 有时我们需要将两个数组合并为一个数组，用ArrayUtils就非常方便，示例如下：
+    private static void testArr() {
+
+        String[] s1 = new String[] { "1", "2", "3" };
+        String[] s2 = new String[] { "a", "b", "c" ,"d" ,"e"};
+        ArrayUtils.shift(s2,2);
+
+        String[] s = (String[]) ArrayUtils.addAll(s1, s2);
+
+        Stream.of(s).forEach(System.out::println);
+
+        String str = ArrayUtils.toString(s);
+        System.out.println(str + ">>" + str.length());
+
+        List<Integer> list=Lists.newArrayList();
+        List<Integer> list1=Lists.newArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list1.add(4);
+        list1.add(5);
+        list1.add(6);
+        list1.addAll(list);
+        list1.stream().forEach(System.out::println);
+        System.out.println(list1+">>"+list1.size());
+    }
+}
--- src/main/java/com/yuyy/temp/Test.java
+++ src/main/java/com/yuyy/temp/Test.java
@@ -1,14 +1,33 @@
 package com.yuyy.temp;
 
+
+
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.*;
-import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 /**
  * @author yuyy
  * @date 2019/9/25 10:56
  */
 public class Test {
+    // 有时我们需要将两个数组合并为一个数组，用ArrayUtils就非常方便，示例如下：
+    private static void testArr() {
+        String[] s1 = new String[] { "1", "2", "3" };
+        String[] s2 = new String[] { "a", "b", "c" };
+
+        String[] s = (String[]) ArrayUtils.addAll(s1, s2);
+
+        Stream.of(s).forEach(System.out::println);
+
+        String str = ArrayUtils.toString(s);
+        System.out.println(str + ">>" + str.length());
+
+
+    }
     public static void main(String[] arg) throws IOException {
+        testArr();
         //读取txt文件
         /*FileReader file=new FileReader("C:\\Users\\Yuyy\\Desktop\\Ego.txt");
         BufferedReader bufferedReader=new BufferedReader(file);
@@ -17,13 +36,13 @@ public class Test {
             System.out.println(line);
         }*/
         //写入txt文件
-        File file1 = new File("C:\\Users\\Yuyy\\Desktop\\out.txt");
+        /*File file1 = new File("C:\\Users\\Yuyy\\Desktop\\out.txt");
         file1.createNewFile();
         FileWriter fileWriter=new FileWriter(file1);
         BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
         bufferedWriter.write("4545sssafaf");
         bufferedWriter.flush();
-        bufferedWriter.close();
+        bufferedWriter.close();*/
         /*for(int i=0;i<10;i++){
             Callable<Integer> callable=new CallableImpl();
             FutureTask<Integer> future=new FutureTask<>(callable);
